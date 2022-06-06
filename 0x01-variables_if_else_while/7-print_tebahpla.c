ceb0018ff6b0d08dd8455d3ddc65fe88499cc9d8
edwardgmuhia <edwardgmuhia@gmail.com>
2022-06-06 06:09:34 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,11 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int i;
 	for(i='z';i>='a';i--)
