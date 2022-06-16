244e56494070a35ba5093e4170b632f81c2f19df
edwardgmuhia <104830396+edwardgmuhia@users.noreply.github.com>
2022-06-16 16:50:24 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,8 +10,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-int _putchar(char)
 void print_number(int n);
+int _putchar(char),
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
